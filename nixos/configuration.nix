5077a67fbde72b5df6b80b0e1262a6a86144c738
Alikay <1401288@gmail.com>
2026-01-16 04:18:51 -0700

    swap to systemd-boot

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -57,17 +57,8 @@
   '';
 
   # Bootloader.
-  boot.loader = {
-    efi = {
-      canTouchEfiVariables = true;
-      efiSysMountPoint = "/boot/efi"; # ← use the same mount point here.
-    };
-    grub = {
-      efiSupport = true;
-      #efiInstallAsRemovable = true; # in case canTouchEfiVariables doesn't work for your system
-      device = "nodev";
-    };
-  };
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 
   # Networking
   networking.hostName = "slipstream-testbed"; # Define your hostname.
