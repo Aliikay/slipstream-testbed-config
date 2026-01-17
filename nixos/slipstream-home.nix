db4ad01f6314f5d831e23b21f705ec40f2518b29
Alikay <1401288@gmail.com>
2026-01-17 00:57:30 -0700

    fix dms

--- nixos/slipstream-home.nix
+++ nixos/slipstream-home.nix
@@ -8,17 +8,18 @@
 {
   imports = [
     inputs.dms.homeModules.dank-material-shell
+    inputs.dms.homeModules.niri
   ];
 
   programs.dank-material-shell = {
     enable = true;
-    # niri = {
-    #   enableKeybinds = true;   # Sets static preset keybinds
-    #   enableSpawn = true;      # Auto-start DMS with niri, if enabled
-    # };
+     niri = {
+       enableKeybinds = true;   # Sets static preset keybinds
+       enableSpawn = true;      # Auto-start DMS with niri, if enabled
+     };
   };
 
-  programs.niri.enable = true;
+  #programs.niri.enable = true;
 
   # BTOP
   programs.btop = {
