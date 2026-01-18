98113ccc27a2a6b5a938454ab7cb70306c4a71fa
Alikay <1401288@gmail.com>
2026-01-18 00:31:05 -0700

    removed script

--- nixos/slipstream-home.nix
+++ nixos/slipstream-home.nix
@@ -41,23 +41,24 @@
       ];
     };
 
-    get-slipstream = {
-      name = "Get Slipstream";
-      genericName = "System Utility";
-      exec = "${pkgs.writeShellScript "get-slipstream-outer" ''
-        alacritty -e sh -c "${pkgs.writeShellScript "get-slipstream-inner" ''
-          rm -r ~/Documents/Slipstream
-          mkdir ~/Documents/Slipstream
-          cp -r /run/user/1000/gvfs/smb-share:server=192.168.1.115,share=public/Link\ to\ Slipstream ~/Documents/Slipstream
-          read -p "Press any key"
-        ''}"
-      ''}";
-      terminal = false;
-      categories = [
-        "System"
-        "Utility"
-      ];
-    };
+    # get-slipstream = {
+    #   name = "Get Slipstream";
+    #   genericName = "System Utility";
+    #   exec = "${pkgs.writeShellScript "get-slipstream-outer" ''
+    #     alacritty -e sh -c "${pkgs.writeShellScript "get-slipstream-inner" ''
+    #       echo "Removing old folder"
+    #       rm -r ~/Documents/Slipstream
+    #       mkdir ~/Documents/Slipstream
+    #       cp -r /run/user/1000/gvfs/smb-share:server=192.168.1.115,share=public/Link\ to\ Slipstream ~/Documents/Slipstream
+    #       read -p "Press any key"
+    #     ''}"
+    #   ''}";
+    #   terminal = false;
+    #   categories = [
+    #     "System"
+    #     "Utility"
+    #   ];
+    # };
   };
 
   # Atuin
